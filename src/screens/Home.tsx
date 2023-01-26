572e2359d8dd0add26e3384c2b23ebaa995a2452
Bruno Tomaz <stomaz.bruno@gmail.com>
2023-01-25 22:17:45 -0300

    Configurate dayjs and create box to habits

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,9 +1,14 @@
-import { View, Text } from 'react-native';
+import { Text, View, ScrollView } from 'react-native'
+
+import { DAY_SIZE, HabitDay } from '../components/HabitDay'
+import { Header } from '../components/Header'
+import { generateRangeDatesFromYearStart } from '../utils/generate-range-between-dates'
 
 const weekDays = ['D', 'S', 'T', 'Q', 'Q', 'S', 'S']
-
-import { HabitDay, DAY_SIZE } from '../components/HabitDay';
-import { Header } from '../components/Header';
+const datesFromYearStart = generateRangeDatesFromYearStart()
+const minimumSummaryDatesSize = 18 * 5
+const amountOfDaysToFill =
+  minimumSummaryDatesSize - datesFromYearStart.length
 
 export function Home() {
   return (
@@ -11,20 +16,36 @@ export function Home() {
       <Header />
 
       <View className="flex-row mt-6 mb-2">
-        {
-          weekDays.map((weekDay, i) => (
-            <Text
-              key={`${weekDay}-${i}`}
-              className="text-zinc-400 text-xl font-bold text-center mx-1"
-              style={{width: DAY_SIZE}}
-            >
-              {weekDay}
-            </Text>
-          ))
-        }
+        {weekDays.map((weekDay, i) => (
+          <Text
+            key={`${weekDay}-${i}`}
+            className="text-zinc-400 text-xl font-bold text-center mx-1"
+            style={{ width: DAY_SIZE }}
+          >
+            {weekDay}
+          </Text>
+        ))}
       </View>
 
-      <HabitDay/>
+      <ScrollView
+        showsVerticalScrollIndicator={false}
+        contentContainerStyle={{ paddingBottom: 100 }}
+      >
+        <View className="flex-row flex-wrap">
+          {datesFromYearStart.map((date) => (
+            <HabitDay key={date.toISOString()} />
+          ))}
+
+          {amountOfDaysToFill > 0 &&
+            Array.from({ length: amountOfDaysToFill }).map((_, index) => (
+              <View
+                key={`${_}-${index}`}
+                className="bg-zinc-900 rounded-lg border-2 m-1 border-zinc-800 opacity-40"
+                style={{ width: DAY_SIZE, height: DAY_SIZE }}
+              />
+            ))}
+        </View>
+      </ScrollView>
     </View>
   )
 }
