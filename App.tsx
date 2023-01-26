8277746cd970fc7e549e94d991adb2478d395c17
Bruno Tomaz <stomaz.bruno@gmail.com>
2023-01-25 22:26:13 -0300

    Configure locale to pt-br

--- App.tsx
+++ App.tsx
@@ -1,3 +1,5 @@
+import './src/lib/dayjs';
+
 import { Inter_400Regular, Inter_600SemiBold, Inter_700Bold, Inter_800ExtraBold, useFonts } from '@expo-google-fonts/inter';
 import { StatusBar } from 'react-native';
 
